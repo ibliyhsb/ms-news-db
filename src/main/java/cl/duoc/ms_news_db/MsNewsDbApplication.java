6636bbd82d8d365973a2a8b6e034086883d86592
ibliyhsb <catd.rojas@duocuc.cl>
2025-12-10 02:15:34 -0300

    proyecto base

---
+++ src/main/java/cl/duoc/ms_news_db/MsNewsDbApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_news_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsNewsDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsNewsDbApplication.class, args);
+	}
+
+}
